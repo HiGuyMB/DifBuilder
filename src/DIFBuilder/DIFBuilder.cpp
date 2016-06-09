ebca75983a49271e7cc7b21f64f89747eaf9427d
HiGuy Smith <higuymb@gmail.com>
2016-06-09 16:06:18 -0400

    Add source

---
+++ src/DIFBuilder/DIFBuilder.cpp
@@ -0,0 +1,351 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2016, HiGuy Smith
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the project nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//-----------------------------------------------------------------------------
+
+#include "DIFBuilder.hpp"
+
+DIF_NAMESPACE
+
+DIFBuilder::DIFBuilder(const DIF &dif) : mScale(1.0f) {
+	//TODO: Extract triangles
+}
+
+void DIFBuilder::addTriangle(const Triangle &triangle) {
+	mTriangles.push_back(triangle);
+}
+
+void DIFBuilder::addTriangle(const Triangle &triangle, const std::string &material) {
+	Triangle tri(triangle);
+
+	//Find the material
+	auto find = std::find(mMaterials.begin(), mMaterials.end(), material);
+	if (find == mMaterials.end()) {
+		tri.material = mMaterials.size();
+		mMaterials.push_back(material);
+	} else {
+		//Distance from the start
+		tri.material = (find - mMaterials.begin());
+	}
+
+	addTriangle(tri);
+}
+
+void DIFBuilder::build(DIF &dif) {
+	typedef U32 PointAssocIndex;
+	typedef U32 DifPointIndex;
+
+	std::map<PointAssocIndex, DifPointIndex> pointAssoc;
+
+	Interior interior;
+	F32 scale = 0.1;
+
+	interior.detailLevel = 0;
+	interior.minPixels = 250;
+	interior.boundingBox = getBoundingBox();
+	interior.boundingSphere = getBoundingSphere();
+
+	interior.hasAlarmState = 0;
+	interior.numLightStateEntries = 0;
+
+	for (const auto &material : mMaterials) {
+		interior.materialName.push_back(material);
+	}
+
+	for (U32 i = 0; i < 256; i ++) {
+		Interior::CoordBin bin;
+		bin.binStart = i;
+		bin.binCount = 1;
+		interior.coordBin.push_back(bin);
+		interior.coordBinIndex.push_back(0);
+	}
+	interior.coordBinMode = 0;
+
+	interior.baseAmbientColor.a = 255;
+	interior.alarmAmbientColor.a = 255;
+
+	interior.extendedLightMapData = 0;
+	interior.lightMapBorderSize = 0;
+
+	//Add vertices to the interior
+	for (const Triangle &triangle : mTriangles) {
+		interior.point.push_back(triangle.points[0].vertex * scale);
+		interior.point.push_back(triangle.points[1].vertex * scale);
+		interior.point.push_back(triangle.points[2].vertex * scale);
+	}
+
+	Interior::Zone zone;
+	zone.portalStart = 0;
+	zone.portalCount = 0;
+	zone.surfaceStart = interior.zoneSurface.size();
+	zone.surfaceCount = mTriangles.size();
+	zone.staticMeshStart = 0;
+	zone.staticMeshCount = 0;
+	zone.flags = 0;
+
+	BoxF box = getBoundingBox();
+	Interior::ConvexHull hull;
+	hull.hullStart = interior.hullIndex.size();
+	hull.hullCount = mTriangles.size();
+	hull.minX = box.minX;
+	hull.minY = box.minY;
+	hull.minZ = box.minZ;
+	hull.maxX = box.maxX;
+	hull.maxY = box.maxY;
+	hull.maxZ = box.maxZ;
+	hull.surfaceStart = interior.solidLeafSurface.size();
+	hull.surfaceCount = mTriangles.size();
+	hull.planeStart = 0;
+	hull.polyListPlaneStart = 0;
+	hull.polyListPointStart = 0;
+	hull.polyListStringStart = 0;
+
+	interior.convexHull.push_back(hull);
+
+	Interior::BSPSolidLeaf leaf;
+	leaf.surfaceIndex = interior.solidLeafSurface.size();
+	leaf.surfaceCount = mTriangles.size();
+
+	interior.bspSolidLeaf.push_back(leaf);
+
+	glm::vec3 center = getBoundingBox().getCenter();
+
+	//Add surfaces
+	for (U32 triIndex = 0; triIndex < mTriangles.size(); triIndex ++) {
+		const Triangle &triangle = mTriangles[triIndex];
+		U32 assoc0 = triIndex * 3 + 0;
+		U32 assoc1 = triIndex * 3 + 1;
+		U32 assoc2 = triIndex * 3 + 2;
+
+		//Averages for normals and planes
+		glm::vec3 averageNormal = getAverageNormal(triangle);
+		F32 distance = getPlaneDistance(triangle, center);
+
+		Interior::Plane plane;
+		plane.normalIndex = interior.normal.size();
+		plane.planeDistance = distance;
+
+		//BSP Node
+		Interior::BSPNode bsp;
+		//todo: probably wrong
+		bsp.planeIndex = ((interior.plane.size() + mTriangles.size() - triIndex) - triIndex);
+		bsp.frontIndex = 0x8000;
+		bsp.backIndex = (triIndex == mTriangles.size() - 1 ? 0xC000 : interior.plane.size() + 1);
+
+		Interior::TexGenEq texGen = getTexGen(triangle);
+
+		//Create a surface
+		Interior::Surface surface;
+		surface.windingStart = interior.index.size();
+		surface.windingCount = 3;
+		surface.planeIndex = interior.plane.size();
+		surface.textureIndex = 0; //todo: more textures?
+		surface.texGenIndex = interior.texGenEq.size();
+		surface.surfaceFlags = 16; //todo: maybe we have some of these
+		surface.fanMask = 7; //todo: ??
+
+		//Lightmap stuff
+		surface.lightMap.finalWord = 0;
+		surface.lightMap.texGenXDistance = 0;
+		surface.lightMap.texGenYDistance = 0;
+		surface.lightCount = 0;
+		surface.lightStateInfoStart = 0;
+		surface.mapOffsetX = 0;
+		surface.mapOffsetY = 0;
+		surface.mapSizeX = 0;
+		surface.mapSizeY = 0;
+		surface.brushId = 0;
+
+		interior.hullPlaneIndex.push_back(interior.plane.size());
+		interior.hullSurfaceIndex.push_back(interior.surface.size());
+		interior.hullIndex.push_back(assoc0);
+		interior.hullIndex.push_back(assoc1);
+		interior.hullIndex.push_back(assoc2);
+
+		//Windings
+		interior.index.push_back(assoc0);
+		interior.index.push_back(assoc1);
+		interior.index.push_back(assoc2);
+
+		interior.polyListPlaneIndex.push_back(interior.plane.size());
+		interior.polyListPointIndex.push_back(assoc0);
+		interior.polyListPointIndex.push_back(assoc1);
+		interior.polyListPointIndex.push_back(assoc2);
+
+		interior.texGenEq.push_back(texGen);
+
+		interior.zoneSurface.push_back(interior.surface.size());
+		interior.solidLeafSurface.push_back(interior.surface.size());
+
+		interior.normal.push_back(averageNormal);
+		interior.plane.push_back(plane);
+		interior.pointVisibility.push_back(-1);
+		interior.normalLMapIndex.push_back(0);
+		interior.alarmLMapIndex.push_back(-1);
+		interior.bspNode.push_back(bsp);
+		interior.surface.push_back(surface);
+	}
+
+	interior.zone.push_back(zone);
+
+	interior.polyListPlaneIndex.push_back(0);
+	interior.polyListPointIndex.push_back(0);
+	interior.polyListStringCharacter.push_back(0);
+	interior.hullEmitStringIndex.push_back(0);
+	interior.convexHullEmitStringCharacter.push_back(0);
+
+	//	std::vector<TexGenEq> texGenEq;
+	//	std::vector<U8> convexHullEmitStringCharacter;
+	//	std::vector<U32> hullIndex;
+	//	std::vector<U16> hullPlaneIndex;
+	//	std::vector<U32> hullEmitStringIndex;
+	//	std::vector<U32> hullSurfaceIndex;
+	//	std::vector<U16> polyListPlaneIndex;
+	//	std::vector<U32> polyListPointIndex;
+	//	std::vector<U8> polyListStringCharacter;
+	
+	dif.interior.push_back(interior);
+}
+
+BoxF DIFBuilder::getBoundingBox() {
+	BoxF box(1e8, 1e8, 1e8, -1e8, -1e8, -1e8);
+	for (const auto &triangle : mTriangles) {
+		box.unionPoint(triangle.points[0].vertex * mScale);
+		box.unionPoint(triangle.points[1].vertex * mScale);
+		box.unionPoint(triangle.points[2].vertex * mScale);
+	}
+	return box;
+}
+
+SphereF DIFBuilder::getBoundingSphere() {
+	BoxF box = getBoundingBox();
+
+	SphereF sphere;
+	glm::vec3 center = box.getCenter();
+	sphere.x = center.x;
+	sphere.y = center.y;
+	sphere.z = center.z;
+	sphere.radius = (box.getMax() - box.getCenter()).length();
+	return sphere;
+}
+
+glm::vec3 DIFBuilder::getAverageNormal(const Triangle &triangle) {
+	const glm::vec3 &point0 = triangle.points[0].vertex;
+	const glm::vec3 &point1 = triangle.points[1].vertex;
+	const glm::vec3 &point2 = triangle.points[2].vertex;
+
+	glm::vec3 crossNorm = glm::normalize(glm::cross(point2 - point1, point2 - point0));
+
+	return crossNorm;
+}
+
+F32 DIFBuilder::getPlaneDistance(const Triangle &triangle, const glm::vec3 &center) {
+	//Use the center of the plane
+	glm::vec3 averagePoint(0);
+	const glm::vec3 &point0 = triangle.points[0].vertex;
+	const glm::vec3 &point1 = triangle.points[1].vertex;
+	const glm::vec3 &point2 = triangle.points[2].vertex;
+	averagePoint += point0;
+	averagePoint += point1;
+	averagePoint += point2;
+	averagePoint /= 3.0f;
+
+	//Get the normal
+	const glm::vec3 &normal = getAverageNormal(triangle);
+
+	//http://mathworld.wolfram.com/Point-PlaneDistance.html
+	//D = n • (x_0 - x_i)
+	// Where x_0 = (0, 0, 0)
+	return glm::dot(-averagePoint * mScale, normal);
+}
+
+glm::vec3 solveSystem(F32 a, F32 b, F32 c, F32 d, F32 e, F32 f, F32 g, F32 h, F32 i, F32 j, F32 k, F32 l) {
+	/*
+	 Solve the system
+
+	 { ax + by + cz = d
+	 { ex + fy + gz = h
+	 { ix + jy + kz = l
+
+	 For (x, y, z)
+
+	 Warning: it's not pretty
+	 */
+	F32 zTop = (l - (i * d / a)) - (((j - (i * b / a)) * (h - (e * d / a))) / (f - (e * b / a)));
+	F32 zBot = (((j - (i * b / a)) * ((e * c / a) - g)) / (f - (e * b / a))) + (k - (i * c / a));
+	F32 z = zTop / zBot;
+
+	F32 y = ((h - (e * d / a)) + (((e * c / a) - g) * z)) / (f - (e * b / a));
+
+	F32 x = (d - (b * y) - (c * z)) / a;
+
+	return glm::vec3(x, y, z);
+}
+
+Interior::TexGenEq getTexGenFromPoints(const glm::vec3 &point0, const glm::vec3 &point1, const glm::vec3 &point2,
+								  glm::vec2 uv0, glm::vec2 uv1, glm::vec2 uv2) {
+	Interior::TexGenEq texGen;
+
+	//Not taking any chances with division by 0
+	F32 min = uv0.x;
+	if (uv0.y < min) min = uv0.y;
+	if (uv1.x < min) min = uv1.x;
+	if (uv1.y < min) min = uv1.y;
+	if (uv2.x < min) min = uv2.x;
+	if (uv2.y < min) min = uv2.y;
+	if (min <= 0.5f) {
+		min = fabsf(min) + 1.0f;
+		uv0.x += min;
+		uv0.y += min;
+		uv1.x += min;
+		uv1.y += min;
+		uv2.x += min;
+		uv2.y += min;
+		texGen.planeX.d = -min;
+		texGen.planeY.d = -min;
+	}
+
+	glm::vec3 xsolve = solveSystem(point0.x, point0.y, point0.z, uv0.x, point1.x, point1.y, point1.z, uv1.x, point2.x, point2.y, point2.z, uv2.x);
+	glm::vec3 ysolve = solveSystem(point0.x, point0.y, point0.z, uv0.y, point1.x, point1.y, point1.z, uv1.y, point2.x, point2.y, point2.z, uv2.y);
+	texGen.planeX.x = xsolve.x;
+	texGen.planeX.y = xsolve.y;
+	texGen.planeX.z = xsolve.z;
+	texGen.planeY.x = ysolve.x;
+	texGen.planeY.y = ysolve.y;
+	texGen.planeY.z = ysolve.z;
+
+	return texGen;
+}
+
+Interior::TexGenEq DIFBuilder::getTexGen(const Triangle &triangle) {
+	return getTexGenFromPoints(triangle.points[0].vertex * mScale,
+							   triangle.points[1].vertex * mScale,
+							   triangle.points[2].vertex * mScale,
+							   triangle.points[0].uv,
+							   triangle.points[1].uv,
+							   triangle.points[2].uv);
+}
+
+DIF_NAMESPACE_END
